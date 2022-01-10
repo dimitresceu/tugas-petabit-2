f9e46a17f5bc951342b9e5bdbd532518521d14b6
Elisabeth Kurnia Andini <92785985+dimitresceu@users.noreply.github.com>
2022-01-10 20:10:32 +0700

    Add files via upload

---
+++ Petabit2.py
@@ -0,0 +1,26 @@
+print("SIMULASI PETABIT")
+print("===================================")
+
+RAM = float(input("Inputkan Kapasitas RAM (dalam Mbps) \n => "))
+BlokUnit = float(input("Inputkan Kapasitas Blok (dalan Kbps) \n => "))
+
+print("Program Tereksekusi")
+print("===================================")
+
+KP1 = float(input("Inputkan Kapasitas Program 1 (dalam Kbps) \n => "))
+KP2 = float(input("Inputkan Kapasitas Program 2 (dalam Kbps) \n => "))
+
+dict = {}
+
+B1 = float(input("Inputkan Jumlah BLok (dalam Kbps) \n => "))
+sum = 0
+i = 1
+while i <= 3:
+  print("Alokasi blok bernilai 1")
+  i+=1
+else:
+  print("Alokasi blok bernilai 0")
+  i+=1
+
+print("Kapasitas RAM \t\t\t: " +str(RAM)+ " Mbps.")
+print("Jumlah Blok \t\t\t: " +str(B1)+ " Kbps.")
